3b0593d1fc42000e5b952044458796428868ebfb
Allan Giovanni Matias Paes <allan.gmp@puccampinas.edu.br>
2025-02-17 09:06:59 -0300

    reorganizating code and adding input in square variable

--- aula_01.py
+++ aula_01.py
@@ -4,19 +4,22 @@
 try:
     number = int(input('Digite o número, irei dizer o antecessor e o sucessor: '))
     print(f'O antecessor de [{number}]  é {number - 1} \nO Sucessor de [{number}] é {number + 1}')
+
+    # Elabore um programa que dado o lado de um quadrado
+    # determine a sua área e seu perímetro
+
+
+    square = int(input('Digite o número para calcular a área do quadrado: '))
+    perimetro = 4 * square
+    area = square**2
+
+    print()
+    print(f'A área de {square} é: {area}²')
+    print('O perímetro de', square, 'é:', perimetro)
+
 except ValueError:
     print('Digite apenas numeros;')
 
-# Elabore um programa que dado o lado de um quadrado
-# determine a sua área e seu perímetro
-
-square = 5
-perimetro = 4 * square
-area = square**2
-
-print(f'A área de {square} é: {area}²')
-
-print('O perímetro de', square, 'é:', perimetro)
 
 
 # Elabore um programa que imprima a parte inteira e o
@@ -24,7 +27,7 @@ print('O perímetro de', square, 'é:', perimetro)
 
 number_one = float(input('Digite um número para dividir: '))
 number_two = float(input('Digite outro número para ser o dividendo: '))
-
+print()
 if (number_one == 0 or number_two == 0):
     print('não é possível dividir por 0')
     exit()
