4c83b7a4de54db75803a3902c5dc3bbeb8689280
Allan Giovanni Matias Paes <allan.gmp@puccampinas.edu.br>
2025-02-17 09:00:11 -0300

    .

--- aula_01.py
+++ aula_01.py
@@ -15,7 +15,7 @@ perimetro = 4 * square
 area = square**2
 
 # area
-print(f'A área de {square} é: {area}² ou {area}')
+print(f'A área de {square} é: {area}²')
 
 # perimetro
 print('O perímetro de', square, 'é:', perimetro)
